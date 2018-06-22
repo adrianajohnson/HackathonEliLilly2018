cbc9801cb3bd4d968d0ebbcce795664f52554d49
Adriana Johnson <johnson_adriana@lilly.com>
2018-06-22 08:40:22 -0400

    Adriana - some html added

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -6,11 +6,34 @@ var router = express.Router();
   res.render('index', { title: 'Express' });
 });*/
 
-router.get('/home', function(req, res, next) {
-  res.render('welcome', { title: 'User' });
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Welcome'});
 });
 
-router.get('/home/question1', function(req, res, next) {
+router.get('/welcome', function(req, res, next) {
+  res.render('welcome', { title: 'Welcome User' });
+});
+
+router.get('/welcome/question1', function(req, res, next) {
   res.render('question1', { title: 'Question1' });
 });
+router.get('/welcome/question1/question2', function(req, res, next) {
+  res.render('question2', { title: 'Question2' });
+});
+router.get('/welcome/question1/question2/question3', function(req, res, next) {
+  res.render('question3', { title: 'Question3' });
+});
+router.get('/welcome/question1/question2/question3/question4', function(req, res, next) {
+  res.render('question4', { title: 'Question4' });
+});
+router.get('/welcome/question1/question2/question3/question4/question5', function(req, res, next) {
+  res.render('question5', { title: 'Question5' });
+});
+router.get('/welcome/question1/question2/question3/question4/question5/question6', function(req, res, next) {
+  res.render('question6', { title: 'Question6' });
+});
+router.get('/welcome/question1/question2/question3/question4/question5/question6/question7', function(req, res, next) {
+  res.render('question7', { title: 'Question7' });
+});
+
 module.exports = router;
